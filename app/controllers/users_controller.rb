7f8dfdd7f48e2e3f3618abad55d8f059506cff4f
naoyasakuma <4869ns@gmail.com>
2021-01-06 16:48:45 +0900

    [update]userコントローラにメソッドを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+      if @user.save
+        redirect_to new_session_path
+      else
+        render :new
+      end
+  end
+
+  def show
+    if current_user.id != @user.id
+    redirect_to user_path(current_user.id)
+    end
+  end
+  
 end
