46d276002aac556bbf9be85e23bd7800e3686880
naoyasakuma <4869ns@gmail.com>
2021-01-06 17:31:22 +0900

    [update]userコントローラーを修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   def new
     @user = User.new
   end
@@ -17,5 +18,9 @@ class UsersController < ApplicationController
     redirect_to user_path(current_user.id)
     end
   end
-  
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
